30cf62042d97d91a248891d49fe246f5ff0020ae
Reddisekhar <reddisekharnaidu143@gmail.com>
2023-08-01 11:56:41 +0530

    third commit...

--- GitHubDemoProject1/src/com/test/Test2.java
+++ GitHubDemoProject1/src/com/test/Test2.java
@@ -7,9 +7,10 @@ public class Test2 {
 		String eligible="yes";
 		
 		if (eligible=="yes" || eligible=="y") {
-			System.out.println("Eligible1");
+			System.out.println("Eligible");
 			System.out.println("Eligible2");
 			System.out.println("Eligible6");
+			System.out.println("Eligible3");
 		}
 		else {
 			System.out.println("Not Eligible");
